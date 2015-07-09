4c5364a0130035cb8ddf796e1621da19e2dd8bf0
Gang Li <gang.li@turn.com>
2015-07-09 15:12:11 -0700

    add demo

--- dist/sb.js
+++ dist/sb.js
@@ -1,27 +1,73 @@
 (function() {
   angular.module('ngSb', ['sbTemplate']).constant('sbDefaults', {
     bufferSize: 10,
-    defaultWait: 2000
+    defaultWait: 400,
+    waitingText: 'Waiting...',
+    mode: {
+      debounce: 'DE',
+      wait: 'WA',
+      queue: 'QU'
+    }
   }).directive('sb', [
-    '$log', 'sbDefaults', function($log, sbDefaults) {
+    '$log', '$q', '$timeout', 'sbDefaults', function($log, $q, $timeout, sbDefaults) {
       return {
         restrict: 'E',
         templateUrl: 'sbTemplate.html',
         scope: {
-          mode: '=',
-          action: '&'
+          mode: '@',
+          action: '&',
+          text: '=',
+          queue: '=?'
         },
         compile: function(ele, attr) {
           var timeout;
           timeout = null;
           return {
             pre: function(scope, ele, attr) {
-              return ele.on('click', function(ev) {
-                console.log('on click');
-                clearTimeout(timeout);
-                timeout = setTimeout(scope.action(), sbDefaults.defaultWait);
-                return ev.preventDefault();
-              });
+              if (scope.mode === sbDefaults.mode.debounce) {
+                return ele.bind('click', function(ev) {
+                  $log.log('on click debounce mode');
+                  clearTimeout(timeout);
+                  timeout = setTimeout(scope.action(), sbDefaults.defaultWait);
+                  return ev.preventDefault();
+                });
+              } else if (scope.mode === sbDefaults.mode.wait) {
+                return ele.bind('click', function(ev) {
+                  var waitFor;
+                  $log.log('on click wait mode');
+                  waitFor = (scope.action())();
+                  ele.children().prop('disabled', true);
+                  scope.text = sbDefaults.waitingText;
+                  scope.$apply();
+                  $q.when(waitFor).then(function() {
+                    ele.children().prop('disabled', false);
+                    scope.text = 'Finished!';
+                    return $timeout(function() {
+                      return scope.text = 'promise';
+                    }, 1000);
+                  }, function() {
+                    return $log.error('Failed to resolve the promise');
+                  });
+                  return ev.preventDefault();
+                });
+              } else if (scope.mode === sbDefaults.mode.queue) {
+                scope.queue = {};
+                return ele.bind('click', function(ev) {
+                  var timestamp, waitFor;
+                  $log.log('on click queue mode');
+                  waitFor = (scope.action())();
+                  timestamp = new Date().getTime();
+                  scope.queue[timestamp] = waitFor;
+                  scope.$apply();
+                  $q.when(waitFor).then(function() {
+                    delete scope.queue[timestamp];
+                    return $log.log('Finished one promise @ ' + timestamp);
+                  }, function() {
+                    return $log.error('Failed to resolve the promise @ ' + timestamp);
+                  });
+                  return ev.preventDefault();
+                });
+              }
             },
             post: function(scope, ele, attr) {}
           };
@@ -40,6 +86,6 @@ try {
 }
 module.run(['$templateCache', function($templateCache) {
   $templateCache.put('sbTemplate.html',
-    '<button>I am a smart button</button>');
+    '<button class="smart-button" ng-class="mode">{{text}}</button>');
 }]);
 })();
--- src/sb.coffee
+++ src/sb.coffee
@@ -2,20 +2,63 @@ angular
 .module 'ngSb', ['sbTemplate']
 .constant 'sbDefaults',
   bufferSize: 10
-  defaultWait: 2000
-.directive 'sb', ['$log', 'sbDefaults', ($log, sbDefaults) ->
+  defaultWait: 400
+  waitingText: 'Waiting...'
+  mode:
+    debounce: 'DE'
+    wait: 'WA'
+    queue: 'QU'
+.directive 'sb', ['$log', '$q', '$timeout', 'sbDefaults', ($log, $q, $timeout, sbDefaults) ->
   restrict: 'E'
   templateUrl: 'sbTemplate.html'
   scope:
-    mode: '='
+    mode: '@'
     action: '&'
+    text: '='
+    queue: '=?'
   compile: (ele, attr) ->
     timeout = null
     pre: (scope, ele, attr) ->
-      ele.on 'click', (ev) ->
-        console.log 'on click'
-        clearTimeout timeout
-        timeout = setTimeout scope.action(), sbDefaults.defaultWait
-        do ev.preventDefault
+      if scope.mode is sbDefaults.mode.debounce
+        ele.bind 'click', (ev) ->
+          $log.log 'on click debounce mode'
+          clearTimeout timeout
+          timeout = setTimeout scope.action(), sbDefaults.defaultWait
+          do ev.preventDefault
+      else if scope.mode is sbDefaults.mode.wait
+        ele.bind 'click', (ev) ->
+          $log.log 'on click wait mode'
+          waitFor = do(do scope.action)
+          ele.children().prop 'disabled', true
+          scope.text = sbDefaults.waitingText
+          do scope.$apply
+          $q.when(waitFor)
+          .then () ->
+            ele.children().prop 'disabled', false
+            scope.text = 'Finished!'
+            $timeout(
+              ->
+                scope.text = 'promise'
+              , 1000)
+          ,
+          () ->
+            $log.error 'Failed to resolve the promise'
+          do ev.preventDefault
+      else if scope.mode is sbDefaults.mode.queue
+        scope.queue = {}
+        ele.bind 'click', (ev) ->
+          $log.log 'on click queue mode'
+          waitFor = do(do scope.action)
+          timestamp = new Date().getTime()
+          scope.queue[timestamp] = waitFor
+          do scope.$apply
+          $q.when(waitFor)
+          .then () ->
+            delete scope.queue[timestamp]
+            $log.log 'Finished one promise @ ' + timestamp
+          ,
+          () ->
+            $log.error 'Failed to resolve the promise @ ' + timestamp
+          do ev.preventDefault
     post: (scope, ele, attr) ->
 ]
